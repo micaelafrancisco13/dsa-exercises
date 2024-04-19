411a589686e1c47e4e2766a86d8b215d7599801c
Micaela Francisco <micaela.francisco@deepdata.com>
2024-04-19 12:04:01 +0800

    implement queue using a linked list

---
+++ part-i/f.queue/pdf-exercises/src/main/java/org/example/LinkedListQueue.java
@@ -0,0 +1,72 @@
+package org.example;
+
+import java.util.Arrays;
+
+public class LinkedListQueue {
+    public static class Node {
+        private final int value;
+        private Node next;
+
+        public Node(int value) {
+            this.value = value;
+        }
+    }
+
+    private Node first;
+    private Node last;
+    private int size;
+
+    public void enqueue(int item) {
+        var node = new Node(item);
+        if (isEmpty())
+            first = last = node;
+        else {
+            last.next = node;
+            last = node;
+        }
+        ++size;
+    }
+
+    public int dequeue() {
+        if (isEmpty())
+            throw new IllegalStateException("Queue is empty");
+
+        var front = first;
+        if (first == last)
+            first = last = null;
+        else {
+            var second = first.next;
+            first.next = null;
+            first = second;
+        }
+        --size;
+
+        return front.value;
+    }
+
+    public int peek() {
+        if (isEmpty())
+            throw new IllegalStateException("Queue is empty");
+        return first.value;
+    }
+
+    public int size() {
+        return size;
+    }
+
+    public boolean isEmpty() {
+        return first == null;
+    }
+
+    @Override
+    public String toString() {
+        int[] items = new int[size];
+        var current = first;
+        var count = 0;
+        while (current != null) {
+            items[count++] = current.value;
+            current = current.next;
+        }
+        return Arrays.toString(items);
+    }
+}
--- part-i/f.queue/pdf-exercises/src/main/java/org/example/Main.java
+++ part-i/f.queue/pdf-exercises/src/main/java/org/example/Main.java
@@ -17,5 +17,19 @@ public class Main {
         arrayDeque.add(100);
         System.out.println(arrayDeque);
         System.out.println("Reversed " + QueueReverser.reverse(arrayDeque, 7));
+
+        System.out.println(' ');
+        
+        var linkedListQueue = new LinkedListQueue();
+        linkedListQueue.enqueue(1);
+        linkedListQueue.enqueue(2);
+        linkedListQueue.enqueue(3);
+        linkedListQueue.enqueue(4);
+        linkedListQueue.enqueue(5);
+        System.out.println(linkedListQueue.dequeue());
+        System.out.println(linkedListQueue);
+        System.out.println(linkedListQueue.peek());
+        System.out.println(linkedListQueue.size());
+        System.out.println(linkedListQueue.isEmpty());
     }
 }
