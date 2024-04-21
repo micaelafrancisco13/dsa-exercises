6acc00ab6e1e85f365200542582406c2f4d142f8
Ela <micaela.francisco.s@bulsu.edu.ph>
2024-04-21 20:44:03 +0800

    polish logs

--- part-i/g.hash-tables/_15-exercise-build-a-hash-table/src/main/java/dsacoursebymosh/Main.java
+++ part-i/g.hash-tables/_15-exercise-build-a-hash-table/src/main/java/dsacoursebymosh/Main.java
@@ -6,20 +6,19 @@ public class Main {
         var entry2 = new Entry(6, "kerwin");
         var entry3 = new Entry(3, "maeve");
         var entry4 = new Entry(4, "ulysses");
-        var entry5 = new Entry(5, "mirava");
+        var entry5 = new Entry(11, "mirava");
         var hashTable = new HashTable(5);
         hashTable.put(entry1);
         hashTable.put(entry2);
         hashTable.put(entry3);
         hashTable.put(entry4);
-//        hashTable.put(entry5);
-        System.out.println(hashTable);
+        hashTable.put(entry5);
         System.out.println(hashTable.remove(3).getValue());
         var removed = hashTable.remove(5);
         System.out.println(removed != null ? removed.getValue() : null);
-        System.out.println(hashTable);
         hashTable.put(new Entry(4, "new ulysses"));
-        System.out.println(hashTable.get(5));
+        System.out.println(hashTable.get(4));
+        System.out.println(hashTable.get(11));
         System.out.println(hashTable);
     }
 }
