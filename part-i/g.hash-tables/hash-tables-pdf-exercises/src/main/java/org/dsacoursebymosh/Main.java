c011891fd3de403944a8433efb1602f3eeb8f4fa
Ela <micaela.francisco.s@bulsu.edu.ph>
2024-04-22 11:47:57 +0800

    implement HashTableExercises.mostFrequent(array)

---
+++ part-i/g.hash-tables/hash-tables-pdf-exercises/src/main/java/org/dsacoursebymosh/Main.java
@@ -0,0 +1,8 @@
+package org.dsacoursebymosh;
+
+public class Main {
+    public static void main(String[] args) {
+        int[] array = { 1, 2, 2, 3, 3, 3, 4 };
+        System.out.println("Most frequent " + (HashTableExercises.mostFrequent(array)));
+    }
+}
