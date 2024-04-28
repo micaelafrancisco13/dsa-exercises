cac31f10e6a2e7e1ba2a2b66c0967e8175976253
Micaela Francisco <micaela.francisco@deepdata.com>
2024-04-28 18:26:52 +0800

    insert another set of values in the tree

--- part-ii/b.binary-trees/_4-exercise-building-a-tree/src/main/java/org/example/Main.java
+++ part-ii/b.binary-trees/_4-exercise-building-a-tree/src/main/java/org/example/Main.java
@@ -7,13 +7,15 @@ public class Main {
         // insert(int value)
         // find(int value): boolean
         var tree = new Tree();
-        tree.insert(7);
-        tree.insert(9);
-        tree.insert(8);
-        tree.insert(4);
-        tree.insert(1);
         tree.insert(10);
+        tree.insert(5);
+        tree.insert(15);
         tree.insert(6);
+        tree.insert(1);
+        tree.insert(8);
+        tree.insert(12);
+        tree.insert(18);
+        tree.insert(17);
         System.out.println(tree.find(13));
         System.out.println(tree.find(9));
     }
