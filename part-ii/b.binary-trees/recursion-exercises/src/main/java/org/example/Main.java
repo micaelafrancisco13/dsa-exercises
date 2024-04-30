0976f4368ea6c636b9feb40631eca85eff0e9199
Micaela Francisco <micaela.francisco@deepdata.com>
2024-04-30 11:31:30 +0800

    implement fibonacci series calculator

--- part-ii/b.binary-trees/recursion-exercises/src/main/java/org/example/Main.java
+++ part-ii/b.binary-trees/recursion-exercises/src/main/java/org/example/Main.java
@@ -3,11 +3,25 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
         System.out.println(calculateSumOfArithmeticSeries(4));
+        System.out.println(calculateFibonacciSeries(6));
     }
 
     public static int calculateSumOfArithmeticSeries(int range) {
         if (range == 1)
             return 1;
         return range + calculateSumOfArithmeticSeries(range - 1);
+    }
+
+    // 0 =
+    // 0 + 1 = 1 . 1
+    // 1 + 1 = 2 . 2
+    // 1 + 2 = 3 . 3
+    // 2 + 3 = 5 . 4
+    // 3 + 5 = 8 . 5
+
+    public static int calculateFibonacciSeries(int length) {
+        if ((length == 0) || (length == 1))
+            return length;
+        return calculateFibonacciSeries(length - 1) + calculateFibonacciSeries(length - 2);
     }
 }
