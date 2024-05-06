36f957b074199599b0c1ca7e733f9d2a71ab24f0
Micaela Francisco <micaela.francisco@deepdata.com>
2024-05-06 13:12:28 +0800

    implement validation of a BST

--- part-ii/b.binary-trees/_15-exercise-validating-binary-search-trees/src/main/java/org/example/Main.java
+++ part-ii/b.binary-trees/_15-exercise-validating-binary-search-trees/src/main/java/org/example/Main.java
@@ -1,9 +1,8 @@
 package org.example;
 
-import java.util.Arrays;
-
 public class Main {
     public static void main(String[] args) {
+        // note that this is a binary search tree
         var tree1 = new Tree();
         tree1.insert(7);
         tree1.insert(4);
@@ -15,13 +14,10 @@ public class Main {
 
         var tree2 = new Tree();
         tree2.insert(2);
-        tree2.insert(4);
-        tree2.insert(9);
         tree2.insert(1);
-        tree2.insert(6);
-        tree2.insert(8);
-        tree2.insert(10);
+        tree2.insert(3);
 
-        System.out.println(tree1.isEqual(tree2));
+        System.out.println(tree1.isBinarySearchTree());
+        System.out.println(tree2.isBinarySearchTree());
     }
 }
