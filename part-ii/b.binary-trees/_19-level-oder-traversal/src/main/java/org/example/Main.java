930a9b82963c377d34395212a4ce6b699f4e7e96
Micaela Francisco <micaela.francisco@deepdata.com>
2024-05-15 18:36:09 +0800

    implement level order traversal

---
+++ part-ii/b.binary-trees/_19-level-oder-traversal/src/main/java/org/example/Main.java
@@ -0,0 +1,23 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+        // note that this is a binary search tree
+        var tree1 = new Tree();
+        tree1.insert(7);
+        tree1.insert(4);
+        tree1.insert(9);
+        tree1.insert(1);
+        tree1.insert(6);
+        tree1.insert(8);
+        tree1.insert(10);
+
+        var tree2 = new Tree();
+        tree2.insert(2);
+        tree2.insert(1);
+        tree2.insert(3);
+
+        System.out.println(tree1.findNodesAtGivenDistance(2));
+        System.out.println(tree1.traverseLevelOrder());
+    }
+}
